2314f3f0aadb369dce1874bd7d15fdfbc222e610
alan-mj-lin <55764881+alan-mj-lin@users.noreply.github.com>
2020-05-21 19:09:08 -0400

    removing server related things

--- shared/SequelizeTypings/models/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './ChurchModel';
-export * from './TaskModel';
-export * from './UserModel';
-export * from './RoleModel';
-export * from './TeamModel';
-export * from './UserRoleModel';
