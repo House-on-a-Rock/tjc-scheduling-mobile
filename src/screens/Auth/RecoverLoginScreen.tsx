acf1ecb28b5881e64e5aa8a37c8c4f3816c1ac99
CedThen <frawg.lehgs@gmail.com>
2020-06-15 22:00:22 -0400

    first draft of reccover login screen

--- src/screens/Auth/RecoverLoginScreen.tsx
+++ src/screens/Auth/RecoverLoginScreen.tsx
@@ -1,13 +1,66 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import { RecoverLoginScreenProps } from '../../shared/models';
+import { CustomInput, BodyText } from '../../shared/components';
 
 export const RecoverLoginScreen = (props: RecoverLoginScreenProps) => {
+    const [email, setEmail] = useState<string>('shaun.tung@gmail.com');
+
     return (
-        <View>
-            <Text>This is the recover login screen</Text>
+        <View style={styles.screen}>
+            <View style={styles.inputContainer}>
+                <BodyText style={styles.inputLabel}>Please enter your email: </BodyText>
+
+                <CustomInput
+                    style={styles.inputStyle}
+                    value={email}
+                    keyboardType={'email-address'}
+                    onChangeText={setEmail}
+                />
+            </View>
+            <View style={styles.buttonContainer}>
+                <View style={styles.buttonStyle}>
+                    <Button
+                        title="Submit"
+                        onPress={() => console.log('submit pressed')}
+                    />
+                </View>
+            </View>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    screen: {
+        height: '100%',
+        width: '100%',
+        backgroundColor: 'white',
+    },
+    inputContainer: {
+        backgroundColor: 'grey',
+        width: '100%',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputLabel: {
+        fontSize: 20,
+    },
+    inputStyle: {
+        // width: '100%',
+        width: 300,
+        paddingVertical: 10,
+        // backgroundColor: 'red',
+    },
+    buttonContainer: {
+        backgroundColor: 'yellow',
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        width: '100%',
+    },
+    buttonStyle: {
+        width: '40%',
+        margin: 2,
+    },
+});
