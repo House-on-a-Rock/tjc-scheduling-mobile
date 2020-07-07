885f027c0ff5eb83f467910602c6525a0ab1ee5c
CedThen <frawg.lehgs@gmail.com>
2020-07-06 22:59:35 -0400

    silenced require cycle warning

---
+++ src/screens/Drawer/TaskDetailsScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Screen } from '../../components/';
+import { Layout, Text } from '@ui-kitten/components';
+
+export const TaskDetailsScreen = (props) => {
+    return (
+        <Screen>
+            <Layout></Layout>
+        </Screen>
+    );
+};
