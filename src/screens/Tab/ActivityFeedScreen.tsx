667d5506d6c335cb2934d70cbd27ecb5c97324cc
CedThen <frawg.lehgs@gmail.com>
2020-09-13 00:41:58 -0400

    fixed reanimation lag for real!

--- src/screens/Tab/ActivityFeedScreen.tsx
+++ src/screens/Tab/ActivityFeedScreen.tsx
@@ -10,10 +10,9 @@ import {
 } from 'react-native';
 import { useSelector } from 'react-redux';
 import { LinearGradient } from 'expo-linear-gradient';
-// import SwipeRow from '../../components/SwipeableItem';
+import SwipeRow from '../../components/SwipeableItem';
 import { NewAssignmentItem } from '../../components/NewAssignments/NewAssignmentItem';
 import { windowWidth } from '../../shared/constants';
-import Swipeable from '../../components/Swipeable';
 
 interface ActivityFeedProps {
     route;
@@ -30,16 +29,11 @@ export const ActivityFeedScreen = (props: ActivityFeedProps) => {
     //grab new assignments instead of tasks
     const newAssignments = useSelector((state) => state.taskReducer.tasks);
     const [data, setData] = useState(newAssignments);
-    const [isScrollable, setIsScrollable] = useState(true);
-    const deleteThreshold = windowWidth * 0.3;
+    const deleteThreshold = windowWidth * 0.4;
 
-    const leftIcon = () => {
-        return <Icon height={20} width={20} name="trash-2-outline" fill="#000000" />;
-    };
-
-    const toggleScrollable = (n) => {
-        setIsScrollable(n);
-    };
+    const leftIcon = (color) => (
+        <Icon height={20} width={20} name="trash-2-outline" fill={color} />
+    );
 
     const deleteItem = (taskId) => {
         const updatedData = data.filter((d) => d.taskId !== taskId);
@@ -50,15 +44,14 @@ export const ActivityFeedScreen = (props: ActivityFeedProps) => {
     };
 
     const renderItem = ({ item, index }) => (
-        <Swipeable
+        <SwipeRow
             swipeThreshold={deleteThreshold}
             onSwipe={deleteItem}
             itemId={item.taskId}
-            toggleScrollParent={toggleScrollable}
             leftIcon={leftIcon}
         >
             <NewAssignmentItem item={item} />
-        </Swipeable>
+        </SwipeRow>
     );
 
     return (
@@ -76,7 +69,6 @@ export const ActivityFeedScreen = (props: ActivityFeedProps) => {
                         data={data}
                         renderItem={renderItem}
                         keyExtractor={(item, index) => item.taskId.toString()}
-                        scrollEnabled={isScrollable}
                         decelerationRate={0.1}
                     />
                 ) : (
